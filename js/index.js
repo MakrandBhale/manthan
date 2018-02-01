49bcc3dac2a476882d99ae37514bbc49ebe91e30
Makarand Bhale <makarandbhale123@gmail.com>
2018-02-01 14:49:27 +0530

    Added firebase

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 function helloWorld(){
   console.log("Hello World");
 }
+
+function init() {
+    var title = document.getElementById('title');
+    var dbref = firebase.database().ref().child('text');
+    dbref.on('value', snap => title.innerText = snap.val());
+}
